7744622ef4297c1f9b4322fc2ccc835e87619e31
Courtney Campany <courtneycampany@gmail.com>
2015-12-07 14:37:49 +1100

    added list of table and figures from chapters
    started notes script

---
+++ reproducibility_notes.R
@@ -0,0 +1,7 @@
+###reproducibility notes
+
+
+###references
+
+#1. subscripts wiht co2:  in bibtex file (in R) replace CO2 with CO_2
+#2. italics use textit{}, for now cannot use this in citation software (medendely or zotero)
